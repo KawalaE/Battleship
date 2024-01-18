8720d5b8b1ba84138661ae140e12b1a991e31bdf
KawalaE <edyta.wer.kawala@gmail.com>
2024-01-18 18:46:31 +0100

    Create allShipSunk function

--- src/gameboard.js
+++ src/gameboard.js
@@ -31,74 +31,90 @@ export default class Gameboard {
     this.tiles[y - 1][x - 1] = value;
   }
 
-  isAvailable(x, y, ship, vertical = false) {
-    if (vertical) {
-      if (!(y - 1 + ship.getLength() - 1 <= this.size - 1)) return false;
-      for (let i = 0; i < ship.getLength(); i += 1) {
-        if (this.getTile(x, y) === "O") {
+  addShip(ship) {
+    this.ships.push(ship);
+  }
+
+  isAvailable(x, y, length, orientation) {
+    if (x < 1 || x > 10 || y > 10 || y < 1) {
+      return false;
+    }
+    if (orientation === "horizontal") {
+      if (x + length - 1 > this.getSize()) {
+        return false;
+      }
+      for (let i = x; i < x + length - 1; i += 1) {
+        if (this.getTile(i, y) === "S") {
           return false;
         }
       }
-    } else {
-      if (!(x - 1 + ship.getLength() - 1 <= this.size - 1)) return false;
-      const arr = this.tiles[y - 1].slice(x - 1, x - 1 + ship.getLength());
-      return arr.indexOf("O") === -1;
+    } else if (orientation === "vertical") {
+      if (y + length - 1 > this.getSize()) {
+        return false;
+      }
+      for (let i = y; i < y + length - 1; i += 1) {
+        if (this.getTile(x, i) === "S") {
+          return false;
+        }
+      }
     }
     return true;
   }
 
-  placeShip(x, y, ship, vertical = false) {
-    let success = false;
-    if (this.isAvailable(x, y, ship, vertical)) {
-      if (vertical) {
-        for (let i = 0; i < ship.getLength(); i += 1) {
-          this.setTile(x, y + 1, "O");
+  placeShip(name, length, x, y, orienatation) {
+    if (this.isAvailable(x, y, length, orienatation)) {
+      const newShip = new Ship(name, length, x, y, orienatation);
+      this.addShip(newShip);
+      if (newShip.getOrientation() === "horizontal") {
+        for (
+          let i = newShip.getXPos();
+          i < newShip.getXPos() + newShip.getLength();
+          i += 1
+        ) {
+          this.setTile(i, newShip.getYPos(), "S");
         }
-        success = true;
-      } else {
-        for (let i = 0; i < ship.getLength(); i += 1) {
-          this.setTile(x - 1, y, "O");
+      } else if (newShip.getOrientation() === "vertical") {
+        for (
+          let i = newShip.getYPos();
+          i < newShip.getYPos() + newShip.getLength();
+          i += 1
+        ) {
+          this.setTile(newShip.getXPos(), i, "S");
         }
-        success = true;
       }
-      const shipData = {
-        shipInfo: ship,
-        xCoordinate: x,
-        yCoordinate: y,
-        isVertical: vertical,
-      };
-      if (success) this.ships.push(shipData);
     }
   }
 
   receiveAttack(x, y) {
+    if (this.getTile(x, y) === "S") {
+      this.getShips().forEach((ship) => {
+        if (ship.getOrientation() === "horizontal") {
+          if (
+            ship.getYPos() === y &&
+            ship.getXPos() <= x &&
+            ship.getXPos() + ship.getLength() - 1 >= x
+          ) {
+            ship.hit();
+          }
+        } else if (
+          ship.getXPos() === x &&
+          ship.getYPos() <= y &&
+          ship.getYPos() + ship.getLength() - 1 >= y
+        ) {
+          ship.hit();
+        }
+      });
+    }
     this.setTile(x, y, "X");
-    this.getShips().forEach((ship) => {
-      if (ship.isVertical && ship.xCoordinate === x) {
-        const lastY = ship.yCoordinate + ship.shipInfo.getLength() - 1;
-        if (y >= ship.yCoordinate && y <= lastY) {
-          ship.shipInfo.hit();
-        }
-      } else if (!ship.isVertical && ship.yCoordinate === y) {
-        const lastX = ship.xCoordinate + ship.shipInfo.getLength() - 1;
-        if (x >= ship.xCoordinate && x <= lastX) {
-          ship.shipInfo.hit();
-        }
-      }
-    });
   }
 
   allShipSunk() {
-    let sunkenCount = 0;
-    if (this.getShipsCount() === 0) return false;
+    let sunkCount = 0;
     this.getShips().forEach((ship) => {
-      if (ship.shipInfo.isSunk()) {
-        sunkenCount += 1;
+      if (ship.isSunk()) {
+        sunkCount += 1;
       }
     });
-    return sunkenCount === this.getShipsCount();
+    return sunkCount === this.getShipsCount();
   }
 }
-let gameboard = new Gameboard(10);
-gameboard.receiveAttack(1,1);
-console.log(gameboard.getBoard());
