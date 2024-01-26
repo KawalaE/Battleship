610249da9a59700fedcc0da8d9a86915c7a22ea4
KawalaE <edyta.wer.kawala@gmail.com>
2024-01-26 12:12:08 +0100

    Create hit class for the cube

--- src/ui.js
+++ src/ui.js
@@ -19,36 +19,42 @@ function createSections() {
   rightSide.classList.add("right-side");
   boardsSection.appendChild(rightSide);
 }
-function boardTitle() {
-  let titleSection = document.createElement('div');
-  titleSection.classList.add("title-sec")
-  document.querySelector(".left-side").appendChild(titleSection);
-  let boardTitle = document.createElement('h1');
-  boardTitle.textContent = "Your board";
-  let enemyBoardPointer = document.createElement('button');
-  enemyBoardPointer.classList.add('human');
-  enemyBoardPointer.textContent = "Your Opponent's Board ->";
-  titleSection.appendChild(boardTitle);
+function boardTitle(player, sideClass, text, btnText) {
+  const titleSection = document.createElement("div");
+  titleSection.classList.add("title-sec");
+  document.querySelector(`.${sideClass}`).appendChild(titleSection);
+  const boardOwner = document.createElement("h1");
+  boardOwner.textContent = `${text}`;
+  const enemyBoardPointer = document.createElement("button");
+  enemyBoardPointer.classList.add(`${player}`);
+  if (player === "computer") {
+    titleSection.classList.add("computer");
+  }
+  enemyBoardPointer.textContent = `${btnText}`;
+  titleSection.appendChild(boardOwner);
   titleSection.appendChild(enemyBoardPointer);
 }
 function displayBoard(player, className, sideClass) {
   const tiles = player.getEnemyTiles();
   const board = document.createElement("div");
   board.classList.add(`${className}`);
-  document.querySelector(`${sideClass}`).appendChild(board);
+  document.querySelector(`.${sideClass}`).appendChild(board);
   tiles.forEach((row, y) => {
     row.forEach((cube, x) => {
       const boardCube = document.createElement("div");
       boardCube.id = `{"x": ${x}, "y": ${y}}`;
       boardCube.classList.add("cube");
+      boardCube.classList.add('hit');
       board.appendChild(boardCube);
     });
   });
 }
+
 createSections();
-console.log(boardTitle());
-displayBoard(human, "board-left", ".left-side");
-displayBoard(computer, "board-right", ".right-side");
+boardTitle("human", "left-side", "Your board", "Enemy Board ->");
+boardTitle("computer", "right-side", "Enemy Board", "<- Your Board");
+displayBoard(human, "board-left", "left-side");
+displayBoard(computer, "board-right", "right-side");
 
 human.play(1, 1);
 computer.playComputer();
