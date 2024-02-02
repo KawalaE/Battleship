6a554d432691febb7fdabe6b2b23dd8121bb460f
KawalaE <edyta.wer.kawala@gmail.com>
2024-02-02 20:46:16 +0100

    Add tests for helper module

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,26 +1,36 @@
-import getRandCoords from "../src/helper";
+import { getRandCoords, getRandomInt } from "../src/helper";
 import Gameboard from "../src/gameboard";
 
-describe("test helper module function", () => {
-  let gameboard = new Gameboard(3);
+describe("test helper functions", () => {
+  let gameboard = new Gameboard("computerBaord", 3);
   beforeEach(() => {
-    gameboard = new Gameboard(3);
+    gameboard = new Gameboard("computerBaord", 3);
   });
-
-  test("define getRandCoords function", () => {
-    expect(typeof getRandCoords).toBe("function");
+  describe("test getRandCoords", () => {
+    test("define getRandCoords function", () => {
+      expect(typeof getRandCoords).toBe("function");
+    });
+    test("invoke Gameboard class method -> getSize()", () => {
+      const getSizeMock = jest.spyOn(gameboard, "getSize");
+      getRandCoords(gameboard);
+      expect(getSizeMock).toHaveBeenCalled();
+    });
+    test("return an array", () => {
+      const returnVal = getRandCoords(gameboard);
+      expect(returnVal).toBeInstanceOf(Array);
+    });
+    test("return numbers in range of 1 - board size", () => {
+      expect(getRandCoords(gameboard)[0]).toBeLessThanOrEqual(3);
+      expect(getRandCoords(gameboard)[1]).toBeGreaterThanOrEqual(1);
+    });
   });
-  test("invoke Gameboard class method -> getSize()", () => {
-    const getSizeMock = jest.spyOn(gameboard, "getSize");
-    getRandCoords(gameboard);
-    expect(getSizeMock).toHaveBeenCalled();
-  });
-  test("return an array", () => {
-    const returnVal = getRandCoords(gameboard);
-    expect(returnVal).toBeInstanceOf(Array);
-  });
-  test("return numbers in range of 1 - board size", () => {
-    expect(getRandCoords(gameboard)[0]).toBeLessThanOrEqual(3);
-    expect(getRandCoords(gameboard)[1]).toBeGreaterThanOrEqual(1);
+  describe("test getRandomInt", () => {
+    test("define getRandomInt function", () => {
+      expect(typeof getRandomInt).toBe("function");
+    });
+    test("get random value in range of 0 - max (max is not included)", () => {
+      expect(getRandomInt(3)).toBeLessThanOrEqual(3);
+      expect(getRandomInt(3)).toBeGreaterThanOrEqual(0);
+    });
   });
 });
